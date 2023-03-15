325ea6725cd9e17c5980a89721e56352fbe93579
wangyl <jk.1848612209>
2023-03-15 11:16:43 +0800

    初始化项目

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import {createApp} from 'vue';
+import router from "./router";
+import ElementPlus from 'element-plus';
+// 需要注意的是，样式文件需要单独引入。
+import 'element-plus/dist/index.css'
+import './style.css';
+import App from './App.vue';
+
+createApp(App)
+  .use(ElementPlus)
+  .use(router)
+  .mount('#app');
---
+++ src/router/index.ts
@@ -0,0 +1,27 @@
+import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router';
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '',
+    redirect: (_) => {
+      return {path: '/login'};
+    }
+  },
+  {
+    path: '/login',
+    name: 'HelloWorld',
+    component: () => import("@/views/Login.vue"),
+  },
+];
+const router = createRouter({
+  history: createWebHistory(''),
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    return {
+      el: '#app',
+      top: 0,
+      behavior: 'smooth'
+    };
+  }
+});
+export default router;
