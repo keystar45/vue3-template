70379e56ce7c1b131cf2af4356fa9dde2d436c3d
wangyl <jk.1848612209>
2023-03-23 19:18:51 +0800

    feat: 浏览器标签页title修改

--- src/router/index.ts
+++ src/router/index.ts
@@ -55,4 +55,12 @@ const router = createRouter({
     };
   },
 });
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title as string;
+  }
+  next();
+});
+
 export default router;
